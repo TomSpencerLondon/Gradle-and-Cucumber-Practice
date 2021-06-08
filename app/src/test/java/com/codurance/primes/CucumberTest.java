70ea68ee1878951e644ae5ecb4110e829356d83e
Tom <thomas.spencer@codurance.com>
2021-06-08 16:04:40 +0100

    add code

---
+++ app/src/test/java/com/codurance/primes/CucumberTest.java
@@ -0,0 +1,8 @@
+package com.codurance.primes;
+
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+public class CucumberTest {
+
+}
---
+++ app/src/test/java/com/codurance/primes/StepDefinitions.java
@@ -0,0 +1,28 @@
+package com.codurance.primes;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.cucumber.java8.En;
+
+public class StepDefinitions implements En {
+  private int number;
+  private int[] result;
+  private PrimeFactors primeFactors = new PrimeFactors();
+
+  public StepDefinitions() {
+    When("I type {int}", (Integer int1) -> {
+      number = int1;
+    });
+    When("I click calculate", () -> {
+      result = primeFactors.of(number);
+    });
+    Then("I get {int}", (Integer int1) -> {
+      assertThat(result).contains(int1);
+    });
+
+    Then("I get {int}, {int}", (Integer int1, Integer int2) -> {
+      assertThat(result).contains(int1, int2);
+    });
+  }
+}
